8a88c14ac93a13b8d71a0e0a0cd79fb178e23d99
Rokudoku <Rokudoku@users.noreply.github.com>
2017-06-29 19:15:46 +1000

    added ask command

--- dekubot.py
+++ dekubot.py
@@ -1,8 +1,6 @@
 from discord.ext.commands import Bot
 import random
 import json
-from time import sleep
-from threading import Timer
 
 dekubot = Bot(command_prefix="!")
 
@@ -17,19 +15,11 @@ async def on_ready():
     print(dekubot.user.name + " online!")
 
 @dekubot.command(pass_context=True)
-async def hello(ctx):
-    """ A friendly greeting.
-    Simply say: Hello <Caller>!.
-    Get the caller's name by allowing the context to be passed through.
-    From the context we can grab message -> author -> display_name.
-    """
-    caller = ctx.message.author.display_name
-    return await dekubot.say(orange_text("Hello {}!".format(caller)))
-
-@dekubot.command(pass_context=True)
 async def roll(ctx):
     """ Rolls a random value from 1-100.
     Returns result in the format: <Caller> rolls <Number> (1-100)
+    Get the caller's name by allowing the context to be passed through.
+    From the context we can grab message -> author -> display_name.
     """
     caller = ctx.message.author.display_name
     number = random.randint(1,100)
@@ -63,6 +53,20 @@ async def rage():
     return await dekubot.say("<https://streamable.com/s9at>\n"
                              "<https://streamable.com/e2az>\n"
                              ":rage:")
+
+@dekubot.command(pass_context=True)
+async def ask(ctx):
+    """ Ask DekuBot a question.
+    Similar to a magic 8 ball.
+    """
+    if len(ctx.message.content) == 4:
+        return await dekubot.say(orange_text("You are not asking me anything..."))
+    replies = ("yep", "of course", "definitely", "you betcha", "most likely", "without a doubt",
+               "maybe", "don't ask me, I'm just a bot", "just Google it", "better not tell you now", "I dunno man",
+               "that's a stupid question", "¯\_(ツ)_/¯", "no", "don't count on it", "it looks doubtful", "lol nah",
+               "yeh nah", "dear god no", "hell no")
+    decision = random.choice(replies)
+    return await dekubot.say(orange_text(decision))
 
 @dekubot.command(pass_context=True)
 async def clear(ctx):
